00937c613d8a4eb9fbfca6ec2797172557f4136f
olafisKem <folafis@googlemail.com>
2024-12-02 16:27:26 +0000

    feat: 2 returns II

--- romanNumerals.ts
+++ romanNumerals.ts
@@ -1,5 +1,22 @@
 import { it } from "@jest/globals";
+// ({}–>nil) no code at all->code that employs nil
+// (nil->constant)
+// (constant->constant+) a simple constant to a more complex constant
+// (constant->scalar) replacing a constant with a variable or an argument
+// (statement->statements) adding more unconditional statements.
+// (unconditional->if) splitting the execution path
+// (scalar->array)
+// (array->container)
+// (statement->recursion)
+// (if->while)
+// (expression->function) replacing an expression with a function or algorithm
+// (variable->assignment) replacing the value of a variable.
 
 export const numberToNumerals = (x: number) : string => {
-    return "I";
+    if (x === 1){
+        return "I";
+    }
+    else {
+        return "II";
+    }
 };
