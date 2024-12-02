80446522dedda8f483d148c923649fc1be56ab63
olafisKem <folafis@googlemail.com>
2024-12-02 15:45:55 +0000

    feat:can return 1

---
+++ romanNumerals.spec.ts
@@ -0,0 +1,9 @@
+import { describe, expect, it } from "@jest/globals";
+import { numberToNumerals } from "./romanNumerals";
+
+describe("numberToNumerals", () => {
+    it("should convert 1 ", () => {
+        expect(numberToNumerals(1)).toEqual("I");
+    })
+})
+
---
+++ romanNumerals.ts
@@ -0,0 +1,5 @@
+import { it } from "@jest/globals";
+
+export const numberToNumerals = (x: number) : string => {
+    return "I";
+};
