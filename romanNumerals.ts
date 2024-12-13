55a6fadf9a1175d4b8bdecaebfc3e4d38e661128
olafisKem <folafis@googlemail.com>
2024-12-13 10:11:11 +0000

    feat: 11 converts to Xi

--- romanNumerals.ts
+++ romanNumerals.ts
@@ -11,8 +11,19 @@
 // (expression->function) replacing an expression with a function or algorithm
 // (variable->assignment) replacing the value of a variable.
 
-// ideas for general logic - numbers 1-3 || number 7-8  return I concacenated * number
-import { it } from "@jest/globals";
+// ideas for general logic - numbers 1-3, number 7-8  - formed by repeating I
+// numbers that do not repeat smaller I's but used I to subtract from a bigger V or X:  4(IV) or 9 (IX)
+// numbers usedto add to a bigger value like V or X:  6(VI) or 11 (XI)
+
+// need to create a map or something
+// const romanNumerals : [number, string][] = [
+//     [10, "X"],
+//     [9, "IX"],
+//     [5, "V"],
+//     [4, "IV"],
+//     [1, "I"]
+// ];
+
 export const numberToNumerals = (x: number) : string => {
     if (x === 1){
         return "I";
@@ -41,7 +52,10 @@ export const numberToNumerals = (x: number) : string => {
     if (x === 9){
         return "IX";
     }
-    else {
+    if (x === 10){
         return "X";
+    }
+    else {
+        return "XI";
     }
 };
